7e11b295a9fab538caaddffd26424e9506861ec3
MinexAutomation <minex.automation@gmail.com>
2020-01-25 01:38:47 -0800

    Added binary- and text-file equality comparer IFileEqualityComparer service implementations.

---
+++ source/R5T.Bedford.Bath/Code/Services/Implementations/BinaryFileEqualityComparer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+using R5T.Bath;
+
+
+namespace R5T.Bedford.Bath
+{
+    public class BinaryFileEqualityComparer : IFileEqualityComparer
+    {
+        private IHumanOutput HumanOutput { get; }
+
+
+        public BinaryFileEqualityComparer(IHumanOutput humanOutput)
+        {
+            this.HumanOutput = humanOutput;
+        }
+
+        public bool Equals(string filePath1, string filePath2)
+        {
+            bool output = true;
+
+            byte[] file1 = File.ReadAllBytes(filePath1);
+            byte[] file2 = File.ReadAllBytes(filePath2);
+
+            output = this.CompareFileLengths(file1, file2);
+            if (!output)
+            {
+                return output;
+            }
+
+            output = this.CompareFileContents(file1, file2);
+            return output;
+        }
+
+        private bool CompareFileContents(byte[] file1, byte[] file2)
+        {
+            bool output = true;
+
+            // Assume at this point that file lengths are equal.
+            int nBytes = file1.Length;
+            for (int iByte = 0; iByte < nBytes; iByte++)
+            {
+                byte value1 = file1[iByte];
+                byte value2 = file2[iByte];
+
+                bool valuesEqual = value1 == value2;
+                if (!valuesEqual)
+                {
+                    output = false;
+
+                    string message = $@"File byte value mismatch: index: {iByte}, file1: {value1.ToString()}, file2: {value2.ToString()}";
+                    this.HumanOutput.WriteLine(message);
+                }
+            }
+
+            return output;
+        }
+
+        private bool CompareFileLengths(byte[] file1, byte[] file2)
+        {
+            int nBytes1 = file1.Length;
+            int nBytes2 = file2.Length;
+
+            bool output = nBytes1 == nBytes2;
+            if (!output)
+            {
+                string message = $@"File byte count mismatch: file1: {nBytes1.ToString()}, file2: {nBytes2.ToString()}";
+                this.HumanOutput.WriteLine(message);
+            }
+
+            return output;
+        }
+    }
+}
---
+++ source/R5T.Bedford.Bath/Code/Services/Implementations/TextFileEqualityComparer.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+
+using R5T.Bath;
+
+
+namespace R5T.Bedford.Bath
+{
+    public class TextFileEqualityComparer : IFileEqualityComparer
+    {
+        private IHumanOutput HumanOutput { get; }
+
+
+        public TextFileEqualityComparer(IHumanOutput humanOutput)
+        {
+            this.HumanOutput = humanOutput;
+        }
+
+        public bool Equals(string filePath1, string filePath2)
+        {
+            bool output = true;
+
+            string[] lines1 = File.ReadAllLines(filePath1);
+            string[] lines2 = File.ReadAllLines(filePath2);
+
+            output = this.CompareLineCounts(lines1, lines2);
+            if (!output)
+            {
+                return output;
+            }
+
+            output = this.CompareLines(lines1, lines2);
+            return output;
+
+        }
+
+        private void DetermineCharacterDifference(string line1, string line2)
+        {
+            // Assume at this point that line lengths are the same.
+            int lineLength = line1.Length;
+            for (int iChar = 0; iChar < lineLength; iChar++)
+            {
+                char line1Char = line1[iChar];
+                char line2Char = line2[iChar];
+
+                bool charMismatch = line1Char == line2Char;
+                if (!charMismatch)
+                {
+                    string message = $@"Char mismatch: index: {iChar.ToString()}, line1: {line1Char.ToString()}, line2: {line2Char.ToString()}";
+                    this.HumanOutput.WriteLine(message);
+                }
+            }
+        }
+
+        private bool CompareLineLength(string line1, string line2)
+        {
+            int line1Length = line1.Length;
+            int line2Length = line2.Length;
+
+            bool output = line1Length == line2Length;
+            if (!output)
+            {
+                string message = $@"Line length mismatch: line1: {line1Length.ToString()}, line2: {line2Length.ToString()}";
+                this.HumanOutput.WriteLine(message);
+            }
+
+            return output;
+        }
+
+        private void DetermineLineDifference(string line1, string line2)
+        {
+            // Assumes at this point the lines are not the same.
+            bool lineLengthEqual = this.CompareLineLength(line1, line2);
+            if (lineLengthEqual)
+            {
+                this.DetermineCharacterDifference(line1, line2);
+            }
+        }
+
+        private bool CompareLines(string[] lines1, string[] lines2)
+        {
+            bool output = true;
+
+            // Assumes at this point the line counts are the same.
+            int nLines = lines1.Length;
+            for (int iLine = 0; iLine < nLines; iLine++)
+            {
+                string line1 = lines1[iLine];
+                string line2 = lines2[iLine];
+
+                bool linesMatch = line1 == line2;
+                if (!linesMatch)
+                {
+                    output = false;
+
+                    string message = $@"Line mismatch. Line {(iLine + 1).ToString()}" + Environment.NewLine + line1 + Environment.NewLine + line2; // +1 to convert from zero-based to one-based line numbers.
+                    this.HumanOutput.WriteLine(message);
+
+                    this.DetermineLineDifference(line1, line2);
+                }
+            }
+
+            return output;
+        }
+
+        private bool CompareLineCounts(string[] lines1, string[] lines2)
+        {
+            int nLines1 = lines1.Length;
+            int nLines2 = lines2.Length;
+
+            bool output = nLines1 == nLines2;
+            if (!output)
+            {
+                string message = $@"File line count mismatch: {nLines1.ToString()} versus {nLines2.ToString()}.";
+                this.HumanOutput.WriteLine(message);
+            }
+
+            return output;
+        }
+    }
+}
